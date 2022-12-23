90badf5d2341b61a851b987863df5334da541a5c
Belen Bahmer-Rada <belenbahmerrada@gmail.com>
2022-12-23 17:09:05 -0600

    form validation

---
+++ src/utils/validateContactForm.js
@@ -0,0 +1,30 @@
+export const validateContactForm = (values) => {
+    const errors = {};
+
+    if (!values.firstName) {
+        errors.firstName = 'Requiered';
+    }else if (values.firstName.lenght <2) {
+        errors.firstName = 'Must be at least 2 characters.';
+    }else if (values.firstName.lenght >15) {
+        errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if (!values.firstName) {
+        errors.lastName = 'Required';
+    }else if (values.lastName.lenght <2) {
+        errors.firstName = 'Must be at least 2 characters.';
+    }else if (values.lastName.lenght >15) {
+        errors.flastName = 'Must be 15 characters or less';
+    }
+
+    const reg = /^\d+$/;
+    if(!reg.test(values.phoneNum)) {
+        errors.phoneNum = 'The phone number should contain only numbers';
+    }
+
+    if (!values.email.includes("@")) {
+        errors.email = 'Emails should contain a @';
+    }
+
+    return errors;
+}
